7d0902d5e1f12d53267b0c2ab919a8af3efeb32e
Praise Akintayo <prakty1205@gmail.com>
2024-06-17 11:08:38 -0700

    added some projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,24 +4,24 @@ import Link from 'next/link';
 import { IoEye } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
   return (
     <div className="hover:shadow-yellow-200 hover:shadow-sm flex flex-col gap-3 bg-slate-800  py-4 px-2 rounded-sm">
       <Image
-        className="bg-slate-700 rounded-md "
+        className="bg-slate-700 rounded-md object-cover "
         width={100}
         height={100}
-        src={"/"}
+        src={props.src}
         alt="project image"
       />
       <div>
-        <p className="text-yellow-200 text-lg">Project Name</p>
-        <p className="text-white text-sm">Project description</p>
+        <p className="text-yellow-200 text-lg">{props.name}</p>
+        <p className="text-white text-sm">{props.description}</p>
       </div>
 
       <div className="flex justify-between text-md">
-        <Link href='https://wa.link/l8cd8e' target='_blank' ><Button backgroundColor="bg-blue-500" label='Github Repo' reactIcon={<FaGithub size={15} className='text-slate-200' />} /></Link>
-        <Link href='https://wa.link/l8cd8e' target='_blank' ><Button backgroundColor="bg-blue-500" label='Live Demo' reactIcon={<IoEye size={15} className='text-slate-200' />} /></Link>
+        <Link href={props.github} target='_blank' ><Button backgroundColor="bg-blue-500" label='Github Repo' reactIcon={<FaGithub size={15} className='text-slate-200' />} /></Link>
+        <Link href={props.link} target='_blank' ><Button backgroundColor="bg-blue-500" label='Live Demo' reactIcon={<IoEye size={15} className='text-slate-200' />} /></Link>
       </div>
     </div>
   );
